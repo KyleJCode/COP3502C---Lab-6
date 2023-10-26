c3df0e217fa088381f9af128658a991ecaba0f06
Phoenix Cushman <phoenixqcushman@gmail.com>
2023-10-26 11:51:08 -0400

    Here's the decode function all done

--- Lab6VersionControl.py
+++ Lab6VersionControl.py
@@ -1,5 +1,11 @@
+def decode(encoded_str):
+    return_str = ""
+    lookup_str = "7890123456"
+    for i in encoded_str:
+        return_str += str(lookup_str[int(i)])
+    return return_str
+
 def main():
     print("Hi")
-
 
 main()
